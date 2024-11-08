c12679ab128d2c86d8470d10db252d569cd15127
Cristian Andres <barcandres91@gmail.com>
2024-11-08 15:19:21 -0500

    validacion configuracion de cors

---
+++ Servidor/picao/src/main/java/com/example/picao/core/security/cors/Cors.java
@@ -0,0 +1,24 @@
+package com.example.picao.core.security.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Cors {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("http://localhost:4200") // URL del frontend Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Permitir cookies/sesiones si es necesario
+            }
+        };
+    }
+}
