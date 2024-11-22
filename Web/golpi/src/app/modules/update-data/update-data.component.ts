91379e0537b39c2bd2ec5648fd5556b50bef69ee
AndreaIsaza28 <44476348+AndreaIsaza28@users.noreply.github.com>
2024-11-22 17:17:48 -0500

    Modulo de actualizar datos
    Se realiza la visual de actualizar datos

--- Web/golpi/src/app/modules/update-data/update-data.component.ts
+++ Web/golpi/src/app/modules/update-data/update-data.component.ts
@@ -1,9 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-update-data',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './update-data.component.html',
   styleUrl: './update-data.component.css',
 })
