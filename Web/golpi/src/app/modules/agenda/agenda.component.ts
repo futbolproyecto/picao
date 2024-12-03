1756291a8e1868f109cc8699884d3cd2aeadb062
AndreaIsaza28 <44476348+AndreaIsaza28@users.noreply.github.com>
2024-12-03 17:07:08 -0500

    Avances
    Se realiza la estructura de las pantallas de registrar cancha y agendamiento

--- Web/golpi/src/app/modules/agenda/agenda.component.ts
+++ Web/golpi/src/app/modules/agenda/agenda.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/components/custom/card/card.component';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-agenda',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, MatIconModule, CommonModule],
   templateUrl: './agenda.component.html',
-  styleUrl: './agenda.component.css'
+  styleUrl: './agenda.component.css',
 })
 export class AgendaComponent {
+  public AdministrarActivo: boolean = true;
+  public nombrePestana: string = 'Administrar agenda';
 
+  activarPestana(pestana: string): void {
+    if (pestana === 'registrar') {
+      this.nombrePestana = 'Administrar agenda';
+    }
+  }
 }
--- Web/golpi/src/app/modules/soccer-field/soccer-field.component.ts
+++ Web/golpi/src/app/modules/soccer-field/soccer-field.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/components/custom/card/card.component';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-soccer-field',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, MatIconModule, CommonModule],
   templateUrl: './soccer-field.component.html',
-  styleUrl: './soccer-field.component.css'
+  styleUrl: './soccer-field.component.css',
 })
 export class SoccerFieldComponent {
+  public RegistrarActivo: boolean = true;
+  public nombrePestana: string = 'Registrar canchas';
 
+  activarPestana(pestana: string): void {
+    if (pestana === 'registrar') {
+      this.nombrePestana = 'Registrar canchas';
+    }
+  }
 }
