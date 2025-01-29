3d3a6aab2ebb5a6212edefdc5fd20416483674e7
AndreaIsaza28 <44476348+AndreaIsaza28@users.noreply.github.com>
2025-01-29 16:22:00 -0500

    modificacion de environment

--- Web/golpi/src/environments/environment.prod.ts
+++ Web/golpi/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // BaseUrl: 'http://localhost:8092',
   BaseUrl: 'https://golpipruebas.up.railway.app/',
   UnprotectedEndpoints: ['authentication/login'],
 };
