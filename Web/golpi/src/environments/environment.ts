5e8b1c02a9d7f7fb5104ef32f45b909a531e87a7
AndreaIsaza28 <44476348+AndreaIsaza28@users.noreply.github.com>
2025-01-30 17:39:54 -0500

    Modifico el environments, ya que quedo comentado la url del servidor

--- Web/golpi/src/environments/environment.ts
+++ Web/golpi/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  BaseUrl: 'http://localhost:8092',
-  // BaseUrl: 'https://golpipruebas.up.railway.app/',
+  // BaseUrl: 'http://localhost:8092',
+  BaseUrl: 'https://golpipruebas.up.railway.app/',
   UnprotectedEndpoints: ['authentication/login'],
 };
